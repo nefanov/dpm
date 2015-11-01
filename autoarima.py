719901f5a04acc3e8de229172c7373e662c676b8
nefanov <kolya-efan@mail.ru>
2015-11-02 02:29:50 +0300

    deps

--- autoarima.py
+++ autoarima.py
@@ -1,6 +1,7 @@
 # configure the environment - because of my local machine options - nevermind
 import sys
 sys.path.append("/usr/local/lib/python2.7/dist-packages")
+sys.path.append("/usr/lib/python2.7/dist-packages")
 # program:
 import scipy as sp
 import numpy
@@ -11,7 +12,7 @@ from rpy2 import *
 import rpy2.robjects as RO
 
 RO.r('library(forecast)')
-
+R0.r('y =scan(file.choose())')
 # use example WWWusage data
 RO.r('fit <- auto.arima(WWWusage)')
 
