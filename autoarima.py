4aeb2e6109a2b6db97c2a7196dee3362bd5fd02e
nefanov <kolya-efan@mail.ru>
2015-11-09 16:05:52 +0300

    arima-3-1-1 added; results of energy consumption during  sysbench added

--- autoarima.py
+++ autoarima.py
@@ -49,4 +49,31 @@ def forecasting_arima(csvname="data.csv"):
 
 T,V = prepare_csv('sysbench_run.log')
 save_csv(T,V)
-print forecasting_arima()
+forecasting_arima()
+#go
+
+#otg = sample.resample('5Min', how=conversion, base=30)
+dataset = read_csv('data.csv')
+dataset.head()
+
+otg=dataset.resample('60Min', how=conversion, base=30)
+itog=otg.describe()
+otg.hist()
+itog
+
+print 'V = %f' % (itog['std']/itog['mean'])
+
+otg1diff = otg.diff(periods=1).dropna()
+m = otg1diff.index[len(otg1diff.index)/2+1]
+r1 = sm.stats.DescrStatsW(otg1diff[m:])
+r2 = sm.stats.DescrStatsW(otg1diff[:m])
+print 'p-value: ', sm.stats.CompareMeans(r1,r2).ttest_ind()[1]
+
+fig = plt.figure(figsize=(12,8))
+ax1 = fig.add_subplot(211)
+fig = sm.graphics.tsa.plot_acf(otg1diff.values.squeeze(), lags=25, ax=ax1)
+ax2 = fig.add_subplot(212)
+fig = sm.graphics.tsa.plot_pacf(otg1diff, lags=25, ax=ax2)
+
+src_data_model = otg[:'2013-05-26']
+model = sm.tsa.ARIMA(src_data_model, order=(3,1,1), freq='10S').fit(full_output=False, disp=0)
